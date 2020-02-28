3a7f958b150c4e7a7affda7afa7bae66361df9b9
Krioks <35567278+Krioks@users.noreply.github.com>
2020-02-28 14:05:18 +0300

    Create GetToken.py

---
+++ GetToken.py
@@ -0,0 +1,51 @@
+import vk
+import os
+import requests
+import time
+import traceback
+import ctypes
+import webbrowser as wb
+import random
+import json
+import codecs
+from urllib.parse import parse_qs
+
+
+
+def GetAcsessToken(app_id):
+    if os.listdir().count('acsess_token.dat'):
+        r = open('acsess_token.dat')
+        acsess_token = r.read()
+        r.close()
+        kernel32 = ctypes.windll.kernel32
+        attr = kernel32.GetFileAttributesW('acsess_token.dat')
+        kernel32.SetFileAttributesW('acsess_token.dat', attr | 2)
+
+    else:
+        auth_url = ("https://oauth.vk.com/authorize?client_id={app_id}&display=page&redirect_uri="
+                    "https://oauth.vk.com/blank.html&scope="
+                    "friends,photos,audio,video,docs,"
+                    "notes,wall,groups, offline"
+                    "&response_type=token&v=5.92&state=42".format(app_id=app_id))
+        wb.open_new_tab(auth_url)
+        redirected_url = input("Paste here url you were redirected:\n")
+        aup = parse_qs(redirected_url)
+        aup['access_token'] = aup.pop('https://oauth.vk.com/blank.html#access_token')
+        s = open('acsess_token.dat', 'w')
+        s.write(aup['acsess_token'][0])
+        s.close()
+        kernel32 = ctypes.windll.kernel32
+        attr = kernel32.GetFileAttributesW('acsess_token.dat')
+        kernel32.SetFileAttributesW('acsess_token.dat', attr | 2)
+        acsess_token = aup['acsess_token'][0]
+    return acsess_token
+
+
+def main():
+	app_id = 6231475
+	acsess_token = GetAcsessToken(app_id)
+	session = vk.Session(acsess_token=acsess_token)
+	api = vk.API(session, v='5.85', lang='ru', timeout=120)
+
+
+main()
